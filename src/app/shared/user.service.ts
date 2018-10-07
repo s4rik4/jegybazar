e5f65702907a01aeaa89107e1e619bf054a4c985
s4rik4 <csabi.sari@gmail.com>
2018-10-08 00:31:52 +0200

    regisztráció bekötése

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -16,16 +16,25 @@ export class UserService {
     if (email === 'angular' && password === 'angular') {
       this._user = new UserModel(UserModel.exampleUser);
       this.isLoggedin = true;
-      console.log(this.isLoggedin);
+      console.log('be vagyunk e lépve', this.isLoggedin);
       this._router.navigate(['/user']);
     }
     return false;
   }
-
+  register(param?: UserModel) {
+    if (param){
+      this._user = new UserModel(param);
+    } else {
+      this._user = new UserModel(UserModel.exampleUser);
+    }
+    this.isLoggedin = true;
+    this._router.navigate(['/user']);
+    console.log('be vagyunk e lépve', this.isLoggedin);
+  }
   logout() {
     delete(this._user);
     this.isLoggedin = false;
-    console.log(this.isLoggedin);
+    console.log('be vagyunk e lépve', this.isLoggedin);
     this._router.navigate(['/home']);
   }
 }
